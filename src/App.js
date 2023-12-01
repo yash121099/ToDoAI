a1f3704821e3465d3fb21ff64c0a72f7dc43817d
yash121099 <thakkaryash1210@gmail.com>
2023-11-30 18:00:45 -0800

    no message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from Yash Thakkar
+          Learn React from Yash B Thakkar
         </a>
       </header>
     </div>
