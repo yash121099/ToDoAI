2f40bd428dce7a188c029c3ef536f78970b2284e
yash121099 <thakkaryash1210@gmail.com>
2023-11-30 17:52:26 -0800

    change commited

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from Yaash
+          Learn React from Yash Thakkar
         </a>
       </header>
     </div>
